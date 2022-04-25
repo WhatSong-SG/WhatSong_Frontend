a8fd4c38fa342bcc183708c4715062de9529abe4
JaeHyun <soehd0889@naver.com>
2022-04-25 11:43:02 +0900

    #7 height 100% setting

--- components/common/Layout/styles.ts
+++ components/common/Layout/styles.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
 
   @media only screen and (min-width: 768px) {
     flex-direction: row;
