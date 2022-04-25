1db240736371ce5cfa84f92fab2aa48d1176ec9a
JaeHyun <soehd0889@naver.com>
2022-04-25 11:01:30 +0900

    #7 worldCup Pages router

---
+++ pages/worldcup.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import WorldCupGenre from "../components/worldcup/worldcup-genre";
+
+const worldCup: NextPage = () => {
+  return <WorldCupGenre />;
+};
+
+export default worldCup;
