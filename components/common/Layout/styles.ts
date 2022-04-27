ae2e7103b54ada00d954f93c3e6ace4b2c88d498
JaeHyun <soehd0889@naver.com>
2022-04-27 15:37:28 +0900

    #7 layout css 스타일 수정

--- components/common/Layout/styles.ts
+++ components/common/Layout/styles.ts
@@ -3,7 +3,6 @@ import styled from "@emotion/styled";
 export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
 
   @media only screen and (min-width: 768px) {
     flex-direction: row;
