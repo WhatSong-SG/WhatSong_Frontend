74f28a593216ee866d6afe553dd3cc5d86a8eca1
JaeHyun <soehd0889@naver.com>
2022-05-02 13:36:21 +0900

    #7 srr:false 설정해서 컴포넌트 불러오기

--- pages/tournament.tsx
+++ pages/tournament.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import WorldCupGenre from "../components/worldcup/worldcup-genre";
+import dynamic from "next/dynamic";
+
+const WorldCuptGenreWithNoSSR = dynamic(
+  () => import("../components/worldcup/worldcup-genre/index"),
+  {
+    ssr: false,
+  }
+);
 
 const tournament: NextPage = () => {
-  return <WorldCupGenre />;
+  return <WorldCuptGenreWithNoSSR />;
 };
 
 export default tournament;
