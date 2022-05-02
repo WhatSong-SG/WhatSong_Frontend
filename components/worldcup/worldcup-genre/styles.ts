18e88c734d6b00f7e5b56dd2b48775e7f75909e6
JaeHyun <soehd0889@naver.com>
2022-05-02 13:36:36 +0900

    #7 style 반응형 설정

--- components/worldcup/worldcup-genre/styles.ts
+++ components/worldcup/worldcup-genre/styles.ts
@@ -2,24 +2,36 @@ import styled from "@emotion/styled";
 
 export const WorldCupMainContainer = styled.main`
   width: 100%;
-  min-width: px;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #f2f2f2;
-  min-height: 680px;
+  @media screen and (max-width: 768px) {
+    height: calc(100vh - 54px);
+  }
 `;
 
 export const GenreArticle = styled.article`
   width: 76%;
-  margin-top: 140px;
+  margin-top: 110px;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+
+  @media screen and (max-width: 1409px) {
+    margin-top: 60px;
+    width: 85%;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-top: 60px;
+    width: 100%;
+    align-items: flex-start;
+  }
 `;
 
 export const HeaderTitleWrapper = styled.section`
-  width: 470px;
+  width: 100%;
   height: 60px;
   display: flex;
   align-items: center;
@@ -34,6 +46,21 @@ export const HeaderTitleWrapper = styled.section`
     justify-content: center;
     align-self: center;
   }
+
+  @media screen and (max-width: 768px) {
+    & > h1 {
+      font-size: 1.875rem;
+      font-weight: bold;
+      margin: 0 7px 0 0;
+    }
+  }
+  @media screen and (max-width: 370px) {
+    & > h1 {
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin: 0 7px 0 0;
+    }
+  }
 `;
 
 export const GenreListContainer = styled.section`
@@ -44,9 +71,23 @@ export const GenreListContainer = styled.section`
 
   @media screen and (max-width: 1894px) {
     justify-content: space-around;
+    margin-top: 80px;
   }
 
   @media screen and (max-width: 1400px) {
     justify-content: center;
+    margin-top: 60px;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-top: 43px;
+    gap: 0;
+    justify-content: space-between;
+  }
+
+  @media screen and (max-width: 720px) {
+    margin-top: 43px;
+    gap: 0;
+    justify-content: space-around;
   }
 `;
