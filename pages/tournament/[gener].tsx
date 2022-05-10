1aedc3ee4e818050c503f0a93cfae0e8c880a482
JaeHyun <soehd0889@naver.com>
2022-05-10 11:43:29 +0900

    #12 gener page에 component 추가

--- pages/tournament/[gener].tsx
+++ pages/tournament/[gener].tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import WorldCupGame from "../../components/worldcup/worldcup-game";
 
 const Gener: NextPage = () => {
   const router = useRouter();
   const { gener } = router.query;
-  
-  return <div>{gener}</div>;
+
+  return <WorldCupGame genre={gener} />;
 };
 
 export default Gener;
