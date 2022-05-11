006812abf787eee7833e3a8d6fbb634e1aea9033
JaeHyun <soehd0889@naver.com>
2022-05-11 20:19:20 +0900

    #12 월드컵 게임 컨테이너 퍼블리싱

---
+++ components/worldcup/worldcup-game/worldcup-game-container/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Main = styled.div`
+  width: 100%;
+  height: 90%;
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
