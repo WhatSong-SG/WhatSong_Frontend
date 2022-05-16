779c643c0dd19a592704bae139c5d6959e9e0e1f
JaeHyun <soehd0889@naver.com>
2022-05-16 16:03:40 +0900

    #12 레이아웃 변경(삭제)

--- components/worldcup/worldcup-game/index.tsx
+++ components/worldcup/worldcup-game/index.tsx
@@ -1,23 +1,14 @@
 import { FC } from "react";
 import * as S from "./styles";
-import dynamic from "next/dynamic";
 import WorldCupGameContainer from "./worldcup-game-container/index";
 
 interface Props {
   genre: any;
 }
 
-const TrophyWithNoSSR = dynamic(() => import("../../../public/icon/Trophy"), {
-  ssr: false,
-});
-
 const WorldCupGame: FC<Props> = ({ genre }) => {
   return (
     <S.DisplayFlexBox>
-      <S.HeaderTitleWrapper>
-        <h1>Choose Your Song</h1>
-        <TrophyWithNoSSR />
-      </S.HeaderTitleWrapper>
       <WorldCupGameContainer />
     </S.DisplayFlexBox>
   );
