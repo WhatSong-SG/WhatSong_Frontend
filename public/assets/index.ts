b0b7b8580af39c86c7d4d0207ed746b6da438e1b
Jun Joy <chojuneseo1@gmail.com>
2022-05-23 09:48:07 +0900

    #10 아이콘 추가

---
+++ public/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as backgroundWave } from "./backgroundWave.svg";
+export { default as grayAppleMusic } from "./gray_apple_music.svg";
+export { default as grayFacebook } from "./gray_facebook.svg";
+export { default as grayInstagram } from "./gray_instagram.svg";
+export { default as graySpotify } from "./gray_spotify.svg";
+export { default as grayTwitter } from "./gray_twitter.svg";
+export { default as grayYoutube } from "./gray_youtube.svg";
