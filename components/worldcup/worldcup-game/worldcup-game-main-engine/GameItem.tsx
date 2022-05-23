1f6b2855ad989af710d6fa58be8789ad33ff8bcc
JaeHyun <soehd0889@naver.com>
2022-05-23 10:06:30 +0900

    #12 음악 제목, 가수 추가

--- components/worldcup/worldcup-game/worldcup-game-main-engine/GameItem.tsx
+++ components/worldcup/worldcup-game/worldcup-game-main-engine/GameItem.tsx
@@ -15,14 +15,46 @@ const GameItem: FC<Props> = () => {
         placeholder="blur"
         alt="게임 사진"
       />
+      <div className="text-center">
+        <h1>Starboy</h1>
+        <p>The Weekend</p>
+      </div>
     </GameItemBox>
   );
 };
 
 const GameItemBox = styled.div`
-  width: 46%;
-  height: 44%;
+  width: clamp(360px, 50%, 750px);
   cursor: pointer;
+  aspect-ratio: 1/1;
+  position: relative;
+
+  .text-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    bottom: 6.2rem;
+    left: 50%;
+    transform: translate(-50%);
+    margin: 0;
+  }
+
+  .text-center > h1 {
+    color: #ffffff;
+    font-weight: 600;
+    font-family: "Poppins", sans-serif;
+    font-size: 2.4rem;
+    margin: 0;
+  }
+
+  .text-center > p {
+    color: #ffffff;
+    font-weight: 100;
+    font-family: "Poppins", sans-serif;
+    font-size: 1.9rem;
+    margin: 0;
+  }
 `;
 
 export default GameItem;
