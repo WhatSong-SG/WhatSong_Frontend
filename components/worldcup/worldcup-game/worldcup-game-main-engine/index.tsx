bdbd3232e62b98bbc57645e296f708400481e16e
JaeHyun <soehd0889@naver.com>
2022-06-16 19:32:49 +0900

    #22 월드컵 게임 api 연동

--- components/worldcup/worldcup-game/worldcup-game-main-engine/index.tsx
+++ components/worldcup/worldcup-game/worldcup-game-main-engine/index.tsx
@@ -1,17 +1,53 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FC, useEffect, useState } from "react";
 import * as S from "./style";
 import GameItemBox from "./GameItem";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  MatchCount,
+  MatchCountMax,
+  MatchInfoData,
+  TournamentId,
+} from "../../../../module/atom/worldcup/worldcup";
+import { matchInfoFind } from "../../../../utils/api/Worldcup";
+import { selectWinnder } from "../../../../utils/api/Worldcup";
+import { useRouter } from "next/router";
 
 interface Props {}
 
 const WorldCupGameMainEngine: FC<Props> = () => {
   const [selectDirection, setSelectDirection] = useState<string>("");
+  const [tournamentId, setTournamentId] = useRecoilState(TournamentId);
+  const [matchCount, setMatchCount] = useRecoilState(MatchCount);
+  const [matchInfoData, setMatchInfoData] = useRecoilState(MatchInfoData);
+  const matchCountMax = useRecoilValue(MatchCountMax);
+
+  const router = useRouter();
+  const { gener } = router.query;
 
   useEffect(() => {
-    console.log(selectDirection);
     if (selectDirection === "left" || selectDirection === "right") {
+      if (selectDirection === "left") {
+        selectWinnder(tournamentId, matchCount, 0)
+          .then(() => {})
+          .catch((err) => {
+            console.log(err);
+          });
+      } else if (selectDirection === "right") {
+        selectWinnder(tournamentId, matchCount, 1)
+          .then(() => {})
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
       setTimeout(() => {
-        setSelectDirection("");
+        if (matchCountMax - 1 === matchCount) {
+          router.push(`/tournament/${gener}/final`);
+        } else {
+          setSelectDirection("");
+          setMatchCount(matchCount + 1);
+        }
       }, 1250);
     }
   }, [selectDirection]);
