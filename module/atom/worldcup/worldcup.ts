1d418db3e75e67af987f411379c6a14ce10ea3e6
JaeHyun <soehd0889@naver.com>
2022-06-16 19:33:27 +0900

    #22 월드컵 recoil atom 추가

--- module/atom/worldcup/worldcup.ts
+++ module/atom/worldcup/worldcup.ts
@@ -1,6 +1,29 @@
 import { atom } from "recoil";
 
-export const TotalRound = atom({
-  key: "TotalRoundAtom",
+export const TournamentId = atom({
+  key: "TournamentIdAtom",
   default: 0,
+});
+
+export const MatchCount = atom({
+  key: "MatchCountAtom",
+  default: 0,
+});
+
+export const MatchCountMax = atom({
+  key: "MatchCountMaxAtom",
+  default: 0,
+});
+
+export const MatchInfoData = atom({
+  key: "MatchInfoDataAtom",
+  default: {
+    current_round: 0,
+    id: 0,
+    is_finish: false,
+    match: 0,
+    match_count: 0,
+    music1: { name: "", artist: "", cover: "" },
+    music2: { name: "", artist: "", cover: "" },
+  },
 });
