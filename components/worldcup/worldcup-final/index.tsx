9c25874dd6c2a2beed6982acc496e7049fdc5f98
JaeHyun <soehd0889@naver.com>
2022-06-16 19:39:27 +0900

    #22 api 연동

--- components/worldcup/worldcup-final/index.tsx
+++ components/worldcup/worldcup-final/index.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import * as S from "./style";
 import GameImage1 from "../../../public/icon/GameImage1.png";
 import Lists from "../../Gerne/Lists";
+import { useRecoilValue } from "recoil";
+import { TournamentId } from "../../../module/atom/worldcup/worldcup";
+import { getfinalWinner } from "../../../utils/api/Worldcup";
+import { FinalWinnerType } from "../../../interface/WorldCup";
 
 interface Props {
   gener: string | string[] | undefined;
@@ -10,16 +14,28 @@ interface Props {
 
 const WorldCupFinal: FC<Props> = ({ gener }) => {
   const List = [1, 2, 3, 4, 5, 6];
-  console.log(gener);
+  const tournamentId = useRecoilValue(TournamentId);
+  const [finalInfo, setFinalInfo] = useState<FinalWinnerType>({
+    name: "",
+    cover: "",
+    artist: "",
+  });
+
+  useEffect(() => {
+    getfinalWinner(tournamentId)
+      .then((res) => setFinalInfo(res))
+      .catch((err) => console.log(err));
+  }, [tournamentId]);
+
   return (
     <S.Main>
       <h1>Final Round</h1>
       <S.MiddleFlexBox>
         <div className="finalImageWrapper">
-          <img src={GameImage1.src} alt="FinalImage" />
+          <img src={finalInfo?.cover} alt="FinalImage" />
           <div className="music-title-artist-center">
-            <h1>Starboy</h1>
-            <p>The Weekend</p>
+            <h1>{finalInfo?.name}</h1>
+            <p>{finalInfo?.artist}</p>
           </div>
         </div>
         <S.ListWrapper>
--- components/worldcup/worldcup-game/worldcup-game-container/index.tsx
+++ components/worldcup/worldcup-game/worldcup-game-container/index.tsx
@@ -1,18 +1,43 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import * as S from "./style";
 import WorldCupTitleWrapper from "./WorldCupTitleData";
 import WorldCupGameMainEngine from "../worldcup-game-main-engine/index";
+import {
+  MatchCount,
+  MatchInfoData,
+  TournamentId,
+} from "../../../../module/atom/worldcup/worldcup";
+import { useRecoilState } from "recoil";
+import { matchInfoFind } from "../../../../utils/api/Worldcup";
 
 interface Props {
   isRoundModalOpen: boolean;
 }
 
 const WorldCupGameContainer: FC<Props> = ({ isRoundModalOpen }) => {
+  const [tournamentId, setTournamentId] = useRecoilState(TournamentId);
+  const [matchCount, setMatchCount] = useRecoilState(MatchCount);
+  const [matchInfoData, setMatchInfoData] = useRecoilState(MatchInfoData);
+
+  useEffect(() => {
+    matchInfoFind(tournamentId, matchCount)
+      .then((res) => {
+        setMatchInfoData(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [tournamentId, matchCount, setMatchInfoData]);
+
   return (
     <S.Main>
       {isRoundModalOpen === false && (
         <>
-          <WorldCupTitleWrapper />
+          <WorldCupTitleWrapper
+            currentRound={matchInfoData.current_round}
+            match={matchInfoData.match}
+            matchCount={matchInfoData.match_count}
+          />
           <WorldCupGameMainEngine />
         </>
       )}
